5e467a0b0efa159a7226d54ab0beaa8e41471e5e
novembergaran <yao_li@yurenedugroup.com>
2017-06-05 18:10:32 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Main></Main>
+    <foooter></foooter>
+  </div>
+</template>
+
+<script>
+import Foooter from './components/Foooter.vue'
+import Main from './components/Main.vue'
+export default {
+  components: { Foooter, Main }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Foooter.vue
@@ -0,0 +1,48 @@
+<template>
+<div id="Foooter" class="foooter">
+        <tabbar>
+      <tabbar-item class="tabbar">
+        <img class="icon" src="../assets/Home.png">
+        <p class="label">Home</p>
+      </tabbar-item>
+      <tabbar-item class="tabbar">
+        <img class="icon" src="../assets/Music.png">
+        <p class="label">Music</p>
+      </tabbar-item>
+      <tabbar-item class="tabbar">
+        <img class="icon" src="../assets/TV.png">
+        <p class="label">Video</p>
+      </tabbar-item>
+    </tabbar>
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+.tabbar{
+  text-align: center;
+  float: left;
+  display: block;
+  background-color: pink;
+  width:33.3333%;
+  height:100%;
+}
+.foooter{
+  display: block;
+  position: fixed;
+  bottom:0px;
+  left:0px;
+  height:10%;
+  width:100%;
+  background-color: #66ccff;
+}
+.icon{
+  width:30px;
+  margin-top:5px;
+}
+.label{
+  font-size: 20px;
+  margin-top:0px; 
+}
+</style>
